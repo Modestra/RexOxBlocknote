ec313c8e4c17e0935d177ad6ac6970e4286dc8b0
Кирилл Карачев <umbrellamixmail@gmail.com>
2025-09-30 16:31:02 +0700

    Создание рабочего окружения

--- src/modchem/commands/__init__.py
+++ src/modchem/commands/__init__.py
@@ -1,12 +1,13 @@
-import sys
-from .command import BaseCommand
+import sys, os
+from .command import ExecuteCommand
+from pathlib import Path
 class ExecuteEnvironment:
     """Класс инициализации виртуальной среды"""
     args = list
     commands = []
     def __init__(self, args):
         self.args = args
-        self.commands.append(BaseCommand(
+        self.commands.append(ExecuteCommand(
             name= "ExecuteExpetiment",
             description= "Запуск виртуальной среды эксперимента"
         ).dict_parser())
@@ -14,6 +15,16 @@ class ExecuteEnvironment:
     def initialize(self):
         if "-h" in self.args:
             self.help_info()
+        elif "ExecuteExperiment" in self.args:
+            self.execute(self.args[1])
+
+    def execute(self, dir: str):
+        os.environ["MODCHEM_INIT"] = os.path.join(os.getcwd(), dir)
+        try:
+            os.mkdir(os.getenv("MODCHEM_INIT"))
+            #file_path = Path(f'{os.getenv("MODCHEM_INIT")}/settings.py')
+        except FileExistsError:
+            sys.stderr.write(f"Путь {os.getenv("MODCHEM_INIT")} уже занят")
 
     def help_info(self):
         """Вывести информацию о текущих командах"""
@@ -23,3 +34,7 @@ class ExecuteEnvironment:
         
     def info(self, command):
         sys.stdout.write(f"\t{command["name"]} - {command["description"]}")
+
+def execute_experiment_environment(argv: str):
+    execute = ExecuteEnvironment(args=argv)
+    execute.initialize()
