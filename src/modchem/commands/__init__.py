5ab6df41d9567834ff3c6721701c9b50af72e999
Modestra <umbrellamixmail@gmail.com>
2025-10-01 03:39:09 +0700

    Переход на setuptools

--- src/modchem/commands/__init__.py
+++ src/modchem/commands/__init__.py
@@ -1,9 +1,10 @@
 import sys, os
 from .command import ExecuteCommand
+from argparse import ArgumentParser
 from pathlib import Path
 class ExecuteEnvironment:
     """Класс инициализации виртуальной среды"""
-    args = list
+    args = ""
     commands = []
     def __init__(self, args):
         self.args = args
@@ -13,13 +14,11 @@ class ExecuteEnvironment:
         ).dict_parser())
         
     def initialize(self):
-        if "-h" in self.args:
-            self.help_info()
-        elif "ExecuteExperiment" in self.args:
-            self.execute(self.args[1])
+        self.execute(self.args)
 
     def execute(self, dir: str):
         os.environ["MODCHEM_INIT"] = os.path.join(os.getcwd(), dir)
+        print(os.getenv("MODCHEM_INIT"))
         try:
             os.mkdir(os.getenv("MODCHEM_INIT"))
             #file_path = Path(f'{os.getenv("MODCHEM_INIT")}/settings.py')
@@ -35,6 +34,10 @@ class ExecuteEnvironment:
     def info(self, command):
         sys.stdout.write(f"\t{command["name"]} - {command["description"]}")
 
-def execute_experiment_environment(argv: str):
-    execute = ExecuteEnvironment(args=argv)
+def execute_experiment_environment():
+    parser = ArgumentParser(description="Initial Experiment Environment")
+    parser.add_argument("project_name", help="Environment name")
+    argv = parser.parse_args()
+    print(argv._get_kwargs()[0][1])
+    execute = ExecuteEnvironment(args=argv._get_kwargs()[0][1])
     execute.initialize()
